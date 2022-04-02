f6d51e75ec1fad66a6d090664855fa0f17832e77
EzaEx <tomlewisint@gmail.com>
2022-04-02 12:50:04 +0100

    Initial project commit

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export var speed = 5; # example
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# handle movement here
+func _physics_process(delta):
+	pass
